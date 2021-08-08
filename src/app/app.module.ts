215da211ad9f588509adc75b20730d0e42e7da74
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2021-08-08 16:01:52 -0400

    update-automated-save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { HomeComponent } from './navpages/home/home.component';
+import { MainComponent } from './navpages/main/main.component';
+import { AuthComponent } from './navpages/auth/auth.component';
+import { Page0Component } from './navpages/page0/page0.component';
+import { AreaChartComponent } from './navpages/page0/area-chart/area-chart.component';
+import { StartChartComponent } from './navpages/page0/area-chart/start-chart/start-chart.component';
+import { DetailChartComponent } from './navpages/page0/area-chart/detail-chart/detail-chart.component';
+import { Page1Component } from './navpages/page1/page1.component';
+import { SearchComponent } from './navpages/search/search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './material-module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    MainComponent,
+    AuthComponent,
+    Page0Component,
+    AreaChartComponent,
+    StartChartComponent,
+    DetailChartComponent,
+    Page1Component,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

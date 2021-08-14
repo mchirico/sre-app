5f32c4cd59d108b5c791cfd0d3f254980de13693
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2021-08-14 07:42:09 -0400

    Adding Table and multiple web sockets

--- Go/server.go
+++ Go/server.go
@@ -1,49 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
-
+	"example.com/m/v2/handles"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"golang.org/x/net/websocket"
 )
-
-func hello(c echo.Context) error {
-	websocket.Handler(func(ws *websocket.Conn) {
-		defer ws.Close()
-		     // Read
-			msg := ""
-			err := websocket.Message.Receive(ws, &msg)
-			if err != nil {
-				c.Logger().Error(err)
-			}
-			fmt.Printf("%s\n", msg)
-		for {
-			// Write
-			result := fmt.Sprintf("{%q: [{%q: %4.2f}, {%q: %4.2f}], %q: %q}","data","p",rand.Float32()*100,"p",rand.Float32()*100,"type","trade")
-			err := websocket.Message.Send(ws, result)
-			if err != nil {
-				c.Logger().Error(err)
-				return
-
-			}
-				fmt.Printf("out: %s\n",result)
-
-
-			
-			time.Sleep(2 * time.Second)
-		}
-	}).ServeHTTP(c.Response(), c.Request())
-	return nil
-}
 
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Static("/", "../public")
-	e.GET("/ws", hello)
+
+	e.GET("/ws", handles.Hello)
+	e.GET("/status", handles.GetStatus)
 	e.Logger.Fatal(e.Start(":1323"))
 }
--- src/app/navpages/main/main.component.ts
+++ src/app/navpages/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject, EMPTY, of, interval} from 'rxjs';
+import { Observable, Subject, EMPTY, of, interval, from} from 'rxjs';
 import { webSocket, WebSocketSubject } from "rxjs/webSocket";
 import { map, catchError, distinctUntilChanged, pairwise, tap, delay, first, takeLast, distinct, switchMap } from 'rxjs/operators';
 
@@ -10,23 +10,47 @@ export interface Trade {
   type: string
 }
 
+export interface Status {
+  data: {
+    name: string,
+    p: number,
+    code: string,
+    n: string
+  } [],
+  type: string
+}
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
+  
   socket$: WebSocketSubject<any> = webSocket({
     // url: 'wss://ws.finnhub.io?token=bsr37a748v6tucpfplbg',
     url: 'ws://localhost:1323/ws',
     openObserver: {
-    next: () => {
-      this.socket$.next({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'});
-    }
-  },
-});
+      next: () => {
+        this.socket$.next({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'});
+      }
+    },
+  });
+  
+  socketStatus$: WebSocketSubject<any> = webSocket({
+    // url: 'wss://ws.finnhub.io?token=bsr37a748v6tucpfplbg',
+    url: 'ws://localhost:1323/status',
+    openObserver: {
+      next: () => {
+        this.socketStatus$.next({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'});
+      }
+    },
+  });
+  
+  
 
+
+  status$: Observable<any> = of(0);
   price$: Observable<any> = of(0);
   direction$: Observable<any> = of('green');
   name = 'stuff'
@@ -36,15 +60,30 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.status$ = this.getLatestStatus();
     this.price$ = this.getLatestPrice();
     this.direction$ = this.getDirection()
   }
 
+  getLatestStatus() {
+    return this.socketStatus$.pipe(
+      tap(d => console.log('Initial:',d)),
+      map((t: Status) => t.type === 'status' && t.data),
+      
+      distinctUntilChanged(),
+      tap(d => console.log('z',d)),
+      catchError(_ => EMPTY)
+    )
+  }
+  
+
   getLatestPrice() {
     return this.socket$.pipe(
+      tap(d => console.log('Initial:',d)),
       map((t: Trade) => t.type === 'trade' && t.data[0].p),
+      
       distinctUntilChanged(),
-      tap(d => console.log(d)),
+      tap(d => console.log('z',d)),
       catchError(_ => EMPTY)
     )
   }
